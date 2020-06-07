c2ec6c6ed60f11dfeeb6b53f8c22a0aa150af3b4
Conner Lane <connerlanestein@gmail.com>
2020-06-06 19:45:05 -0700

    lower level floor

--- js/levels/level1.js
+++ js/levels/level1.js
@@ -1,7 +1,7 @@
 
 var LEVEL_1 = new Level(
     1000, // width
-    1700, // height
+    1900, // height
     new ParallaxBackground(["bg1_1", "bg1_2", "bg1_3"], [0.05, 0.1, 0.15]), // background
     -350, // player start pos. X
     700, // player start pos. Y
