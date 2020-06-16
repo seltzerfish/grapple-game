44e374a751b5293685814ab708e2ceff53068dc2
Conner Lane <connerlanestein@gmail.com>
2020-06-15 19:03:42 -0700

    clarifying comments and updated README

--- js/solids/partial_deadly_solid.js
+++ js/solids/partial_deadly_solid.js
@@ -1,3 +1,8 @@
+/**
+ * This is a bit of a hacky class. I doubt it will make it into the final version of the game,
+ * but it's useful for level design.
+ */
+
 class PartialDeadlySolid extends Solid{
     constructor(x, y, w, h, topDeadly=false, bottomDeadly=true, leftDeadly=true, rightDeadly=true, srcImage) {
         super(x, y, w, h, srcImage);
