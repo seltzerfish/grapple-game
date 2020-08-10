ea4de960514405739dab4907442d83b1abb512b8
ahirschberg <ahirschberg@users.noreply.github.com>
2020-08-09 23:24:26 -0700

    Add support for deadly solid, dog, and jupiter in level editor.

--- js/levels/level1.js
+++ js/levels/level1.js
@@ -1,16 +1,26 @@
 var LEVEL_1 = (function() {
-  "use strict"
+  "use strict";
 
   // ideally would have a js compiler that could pull in json deps automatically.
   // For now, this is a copy-paste from leveldev/parse_test.json
-  let level_json = {"solids": [{"x": 0, "y": 900, "width": 250, "height": 100}, {"x": 200, "y": 520, "width": 100, "height": 100}, {"x": 470, "y": 240, "width": 470, "height": 100}, {"x": 780, "y": 500, "width": 160, "height": 100}]}
+  let level_json = {"solids": [{"x": 680, "y": 720, "width": 100, "height": 80, "shape": 4}, {"x": 234, "y": 940, "width": 846, "height": 488, "shape": 1}, {"x": 1077, "y": 1410, "width": 1510, "height": 22, "shape": 1}, {"x": 1051, "y": 558, "width": 166, "height": 133, "shape": 1}, {"x": 2009, "y": 94, "width": 166, "height": 133, "shape": 1}, {"x": 2941, "y": -662, "width": 166, "height": 133, "shape": 3}, {"x": 3486, "y": -392, "width": 166, "height": 133, "shape": 1}, {"x": 1639, "y": 380, "width": 73, "height": 73, "shape": 2}, {"x": 2517, "y": -210, "width": 73, "height": 73, "shape": 2}, {"x": 2625, "y": -7, "width": 56, "height": 302, "shape": 4}]}
 
-  let solids = level_json["solids"].map(obj => new Solid(obj.x, obj.y, obj.width, obj.height))
+  let solids = level_json["solids"].map(obj => {
+    if (obj.shape == 1) {
+      return new Solid(obj.x, obj.y, obj.width, obj.height);
+    } else if (obj.shape == 2) {
+      return new Jupiter(obj.x, obj.y);
+    } else if (obj.shape == 3) {
+      return new Dog(obj.x, obj.y);
+    } else if (obj.shape == 4) {
+      return new DeadlySolid(obj.x, obj.y, obj.width, obj.height);
+    }
+  })
   return new Level(
-      1000, // width
-      1900, // height
+      9715, // width
+      3128, // height
       new ParallaxBackground(["bg1_1", "bg1_2", "bg1_3"], [0.05, 0.1, 0.15]), // background
-      27, // player start pos. X
+      777, // player start pos. X
       848, // player start pos. Y
       0.25, // gravity constant
       solids, // solids
@@ -42,4 +52,5 @@ var old_LEVEL_1 = new Level(
     ],
     [] // actors
 );
+
 
--- leveldev/parse_svg.py
+++ leveldev/parse_svg.py
@@ -1,8 +1,34 @@
 import re
+import enum
 import sys
 import json
 import dataclasses
 from bs4 import BeautifulSoup
+
+def str_to_rounded_int(s):
+  return int(round(float(s)))
+
+def get_fill(svg_shape):
+  shape_style = svg_shape['style'].lower()
+  hex_color = re.search(r'(?:^|;)fill:#([a-f0-9]{6})(?:$|;)', shape_style)
+  if not hex_color:
+    raise ValueError('No fill in style string {}'.format(shape_style))
+  return hex_color.group(1)
+
+
+class Shape(enum.IntEnum):
+  RECT = 1
+  CIRCLE = 2
+  DOG = 3
+  DEADLY = 4
+
+
+COLOR_CODES = {
+  '808080': Shape.RECT,  # 50% gray
+  'ff00ff': Shape.DEADLY, # Magenta
+  'ffcc00': Shape.DOG,  # golden yellow ish
+}
+
 
 @dataclasses.dataclass(frozen=True)
 class Solid:
@@ -10,22 +36,36 @@ class Solid:
   y: int
   width: int
   height: int
+  shape: Shape
 
-  @classmethod
-  def from_svg_rect(cls, rect):
-    return cls(x=int(rect['x']),
-               y=int(rect['y']),
-               width=int(rect['width']),
-               height=int(rect['height']))
+
+def solid_from_rect(rect):
+  return Solid(x=str_to_rounded_int(rect['x']),
+             y=str_to_rounded_int(rect['y']),
+             width=str_to_rounded_int(rect['width']),
+             height=str_to_rounded_int(rect['height']),
+             shape=COLOR_CODES[get_fill(rect)])
+
+def solid_from_circle(circle):
+  return Solid(x=str_to_rounded_int(circle['cx']),
+             y=str_to_rounded_int(circle['cy']),
+             width=str_to_rounded_int(circle['r']),
+             height=str_to_rounded_int(circle['r']),
+             shape=Shape.CIRCLE)
+
+CONVERTERS = {
+  'rect': solid_from_rect,
+  'circle': solid_from_circle
+}
 
 
 def parse_solids(soup):
   soup_solids = soup.svg.find('g', id='layer1')
   print(soup_solids.prettify())
   solids = []
-  for rect in soup_solids.find_all('rect'):
-    print(rect)
-    solids.append(Solid.from_svg_rect(rect))
+  for shape in soup_solids.find_all(['rect', 'circle']):
+    print(shape)
+    solids.append(CONVERTERS[shape.name](shape))
   return solids
 
 
